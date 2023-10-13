2964aa5f83fe84f794c6181ec40051df432cb62a
clementine-mini <146268774+clementine-mini@users.noreply.github.com>
2023-10-13 13:55:25 -0400

    Cours 06

---
+++ Cours06/ExercicePositionPlusGrandeTemperature/main.c
@@ -0,0 +1,23 @@
+/**
+ * EXERCICE
+ * Écrire un programme (main) qui demande à l'usager de saisir le nombre de températures
+ * dont il dispose et qui lui demande ensuite de saisir l'ensemble des températures,
+ * qui seront stockées dans un tableau.
+ *
+ * Supposez que le nombre de températures dont l'usager dispose n'excèdera jamais 100.
+ *
+ * Votre programme doit ensuite afficher **la position** (dans le tableau) de la température
+ * la plus élevée.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(void) {
+
+
+
+    return EXIT_SUCCESS;
+}
