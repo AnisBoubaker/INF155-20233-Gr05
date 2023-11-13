68553b3a97ee67cb85219a72fcc3e5381273bd53
Anis Boubaker <anis@boubaker.ca>
2023-11-13 10:29:00 -0500

    Cours 09

---
+++ Cours09/Exercice_Etudiants/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+ * Définir le type étudiant, où pour chaque étudiant-e on veut stocker les informations
+ * suivantes:
+ *
+ * - Le nom
+ * - Le prénom
+ * - Le code permanent
+ * - La note du tp1
+ * - La note du tp2
+ * - La note intra
+ * - La note du final
+ */
+
+
+int main() {
+    /*
+     * Créer un tableau de 40 étudiants, et remplir les informations du premier etudiant
+     * du tableau (informations de votre choix)
+     */
+
+
+    return 0;
+}
